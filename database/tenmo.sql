72f01d495f603a3ef58c1c255b803bdc438e048f
Stephanie <kikkboxx@gmail.com>
2023-07-05 18:46:47 -0400

    Push SQL update

--- database/tenmo.sql
+++ database/tenmo.sql
@@ -43,6 +43,13 @@ CREATE TABLE status (
 	CONSTRAINT UQ_status_name UNIQUE (status_name)
 );
 
+CREATE TABLE transfer_type (
+	type_code serial NOT NULL,
+	type_name varchar(50) NOT NULL,
+	CONSTRAINT PK_type PRIMARY KEY (type_code),
+	CONSTRAINT UQ_type_name UNIQUE (type_name)
+);
+
 CREATE SEQUENCE seq_transfer_id
   INCREMENT BY 1
   START WITH 5001
@@ -50,20 +57,18 @@ CREATE SEQUENCE seq_transfer_id
 
 CREATE TABLE transfer(
 	transfer_id int NOT NULL DEFAULT nextval('seq_transfer_id'),
-	transfer_type varchar(50) NOT NULL,
-	transfer_amount numeric NOT NULL,
-	transfer_status int NOT NULL,
-	CONSTRAINT PK_transfer PRIMARY KEY (transfer_id),
-	CONSTRAINT FK_transfer FOREIGN KEY (transfer_status) REFERENCES status (status_code)
-);
-
-CREATE TABLE user_transfer(
-	transfer_id int NOT NULL,
 	to_user int NOT NULL,
 	from_user int NOT NULL,
-	CONSTRAINT PK_user_transfer PRIMARY KEY(transfer_id, to_user, from_user),
+	transfer_type varchar NOT NULL,
+	transfer_amount numeric NOT NULL,
+	transfer_status varchar NOT NULL,
+	CONSTRAINT PK_transfer PRIMARY KEY (transfer_id),
 	CONSTRAINT FK_user_transfer_to FOREIGN KEY (to_user) REFERENCES tenmo_user (user_id),
 	CONSTRAINT FK_user_transfer_from FOREIGN KEY (from_user) REFERENCES tenmo_user (user_id),
-	CONSTRAINT FK_user_transfer_tfer_id FOREIGN KEY (transfer_id) REFERENCES transfer (transfer_id)
+	CONSTRAINT FK_transfer_status FOREIGN KEY (transfer_status) REFERENCES status (status_name),
+	CONSTRAINT FK_transfer_type FOREIGN KEY (transfer_type) REFERENCES transfer_type (type_name)
 );
+
+
+	
 
